b90603f2d48510882cdbac8eeeb5e3d91cf7bc66
Kris Szafranski <kdszafranski@gmail.com>
2016-02-04 11:24:34 -0600

    initial commit, all working

---
+++ README.md
@@ -0,0 +1,3 @@
+# API Lecture Notes
+
+Don't forget to run 'npm install' in your console.
---
+++ server/public/scripts/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // do first
+    //loadData();
+
+    // load on event
+    $('#load-data').on('click', loadData);
+});
+
+function loadData() {
+    $.ajax({
+        type: 'GET',
+        url: '/data',
+        success: function(data) {
+            console.log(data);
+            $.each(data.people, function(i, person) {
+                $('#ajax-data').append('<div class="person"></div>');
+                console.log(person.name);
+                var $el = $('#ajax-data').children().last();
+
+                $el.append('<h2>' + person.name + '</h2>');
+                $el.append('<p>' + person.position + '</p>');
+                $el.append('<p>' + person.location + '</p>');
+                $el.append('<img src="' + person.imageURL + '"></p>');
+            })
+        },
+        error: function() {
+            console.log("oops, something went wrong");
+        }
+    });
+
+}
