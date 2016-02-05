33a03801a911532b7d5177fe49b41ce9fcba3d4b
Kris Szafranski <kdszafranski@gmail.com>
2016-02-05 12:32:43 -0600

    upated assingment data file with iota cohort answers

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require('path');
-var data = require('./public/data/theta');
+var data = require('./public/data/iota');
 
 app.set("port", process.env.PORT || 5000);
 
