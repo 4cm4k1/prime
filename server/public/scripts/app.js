ecce1d1849815a08677f5adbd2043dc8cfd777c2
Kris Szafranski <kdszafranski@gmail.com>
2016-02-05 08:25:20 -0600

    complete version from lecture

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    // do first
-    loadData();
+    //loadData();
 
-    // load on event
     $('#load-data').on('click', loadData);
 });
 
@@ -11,21 +9,22 @@ function loadData() {
         type: 'GET',
         url: '/data',
         success: function(data) {
-            console.log(data);
+            //console.log(data);
             $.each(data.people, function(i, person) {
                 $('#ajax-data').append('<div class="person"></div>');
-                console.log(person.name);
                 var $el = $('#ajax-data').children().last();
-
                 $el.append('<h2>' + person.name + '</h2>');
                 $el.append('<p>' + person.position + '</p>');
                 $el.append('<p>' + person.location + '</p>');
-                $el.append('<img src="' + person.imageURL + '"></p>');
-            })
+                $el.append('<img src="' + person.imageURL +'" />');
+            });
         },
         error: function() {
-            console.log("oops, something went wrong");
+            // do logic here on fail
+            console.log('oops, error!');
+        },
+        timeout: function() {
+
         }
     });
-
 }
