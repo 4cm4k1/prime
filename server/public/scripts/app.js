ed6315cb7e99b7d3663fe50a9ec153bd06920520
4cm4k1 <4cm4k1@gmail.com>
2016-02-08 04:24:17 -0600

    Finish Weekend Challenge

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,3 +1,8 @@
+var $peopleObject;
+var $personArray;
+var currentlyFocusedPerson;
+var intervalID;
+
 $(document).ready(function(){
     getData();
 });
@@ -7,11 +12,91 @@ function getData(){
         type: "GET",
         url:"/data",
         success: function(data) {
-            console.log(data);
+
+            populateDOM(data);
+
+            initializePage();
+
+            $('button').on('click', function () {
+                buttonClickEvent(this);
+            });
+
+            startAutoAdvance();
         },
         error: function() {
             console.log('ERROR: Unable to contact the server.');
         }
 
     });
+}
+
+function populateDOM(data){
+    $peopleObject = data;
+    $personArray = $peopleObject.personArray;
+    $.each($personArray, function(i, person) {
+        $('#focus').append(
+            '<div class="list-group" id="person' + i + '">' +
+            '<a class="list-group-item" style="background: #efefef url(\'' + person.imageURL + '\')' +
+            'no-repeat center; background-size: cover; height: 555px;">' +
+            '<h2 class="list-group-item-heading" style="color: #efefef; text-shadow: 1px 1px #333;">' + person.name +
+            '</h2></a><ul><li class="list-group-item"><h3>Favorite movies</h3>' + person.favoriteMovie1 + '<br/>' +
+            person.favoriteMovie2 + '</li><li class="list-group-item"><h3>Favorite song</h3>' + '<br/>' +
+            person.favoriteSong + '</li></ul>' + '</div>');
+        $('.btn-group').append('<button class="btn btn-default" id ="' + i + '">' + (i+1) + '</button>');
+    });
+}
+
+function initializePage () {
+    currentlyFocusedPerson = 0;
+    $('#person' + currentlyFocusedPerson).addClass('focused');
+    $('#focus div:not(".focused")').addClass('unfocused');
+}
+
+function buttonClickEvent (element) {
+    if (isNaN(element.id)) {
+        if (element.id == 'left-arrow' && currentlyFocusedPerson > 0) {
+            $('#person' + currentlyFocusedPerson).fadeOut(1000);
+            currentlyFocusedPerson--;
+            $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+        } else if (element.id == 'right-arrow' && currentlyFocusedPerson < $personArray.length-1) {
+            $('#person' + currentlyFocusedPerson).fadeOut(1000);
+            currentlyFocusedPerson++;
+            $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+        } else if (element.id == 'right-arrow' && currentlyFocusedPerson == $personArray.length-1) {
+            $('#person' + currentlyFocusedPerson).fadeOut(1000);
+            currentlyFocusedPerson = 0;
+            $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+        } else if (element.id == 'left-arrow' && currentlyFocusedPerson == 0) {
+            $('#person' + currentlyFocusedPerson).fadeOut(1000);
+            currentlyFocusedPerson = $personArray.length-1;
+            $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+        }
+    } else {
+        $('#person' + currentlyFocusedPerson).fadeOut(1000);
+        currentlyFocusedPerson = parseInt(element.id);
+        $('#person' + currentlyFocusedPerson).delay(1000).fadeIn();
+        console.log(currentlyFocusedPerson);
+    }
+    clearTimer();
+    startAutoAdvance();
+}
+
+function startAutoAdvance () {
+    intervalID = setInterval(autoAdvance, 10000);
+}
+
+function autoAdvance () {
+    if (currentlyFocusedPerson < $personArray.length-1) {
+        $('#person' + currentlyFocusedPerson).fadeOut(1000);
+        currentlyFocusedPerson++;
+        $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+    } else if (currentlyFocusedPerson == $personArray.length-1) {
+        $('#person' + currentlyFocusedPerson).fadeOut(1000);
+        currentlyFocusedPerson = 0;
+        $('#person' + currentlyFocusedPerson).delay(1000).fadeIn(1000);
+    }
+}
+
+function clearTimer () {
+    clearInterval(intervalID);
 }
