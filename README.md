132d0c7e1b5af2e0dfdc88d5eb72e9744db4e083
Anthony Maki <4cm4k1@gmail.com>
2016-06-27 04:08:07 -0500

    Challenge complete

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # Prime Weekend Challenge 1
 
-##Hey Primers!
+## Overview
 
-For this weekend, you will be creating an application that will process insurance claims. You will be provided an array of 5 items, where you will use the constructor function to create 5 more, combining to the two to have one array of 10 items.  
+This project is the first weekend challenge at Prime Digital Academy. The objective was to create a simple application to process insurance claims. Technologies used include Bootstrap and jQuery.
 
-You will need to create two functions, one will determine what percentage (per rules below) will be covered and another will determine what that amount will be (per the visitCost value). Within your cost determining method, console out a message in the format ('Paid out $______ for _______') where the second statement is the patient name. 
+## Instructions Given
 
-After that is complete, console out the entire amount paid out.
-
-Numbers should be rounded to the nearest whole number!
-
-###Rules
-Optical - covers 0%
-
-Specialist - covers 10%
-
-Emergency - covers 100%
-
-Primary Care - covers 50%	
-
-##Hard Mode
-Append to the DOM!
-
-##Pro Mode
-Style appended content with CSS!
+> ## Hey Primers!
+>
+> For this weekend, you will be creating an application that will process insurance claims. You will be provided an array of 5 items, where you will use the constructor function to create 5 more, combining to the two to have one array of 10 items.  
+>
+> You will need to create two functions, one will determine what percentage (per rules below) will be covered and another will determine what that amount will be (per the visitCost value). Within your cost determining method, console out a message in the format ('Paid out $______ for _______') where the second statement is the patient name.
+>
+> After that is complete, console out the entire amount paid out.
+>
+> Numbers should be rounded to the nearest whole number!
+>
+> ## Rules
+> Optical - covers 0%
+>
+> Specialist - covers 10%
+>
+> Emergency - covers 100%
+>
+> Primary Care - covers 50%
+>
+> ## Hard Mode
+> Append to the DOM!
+>
+> ## Pro Mode
+> Style appended content with CSS!
--- app.js
+++ app.js
@@ -1,23 +1,97 @@
-var claim1 = new Claim("John Doe", "Specialist", 1100);
+var claim1 = new Claim('John Doe', 'Specialist', 1100);
 
-var claim2 = new Claim("Jane Doe", "Optical", 100);
+var claim2 = new Claim('Jane Doe', 'Optical', 100);
 
-var claim3 = new Claim("Joe Johnson", "Emergency", 31000);
+var claim3 = new Claim('Joe Johnson', 'Emergency', 31000);
 
-var claim4 = new Claim("Sharon Smith", "Emergency", 1300);
+var claim4 = new Claim('Sharon Smith', 'Emergency', 1300);
 
-var claim5 = new Claim("Steve Wright", "Primary Care", 770);
+var claim5 = new Claim('Steve Wright', 'Primary Care', 770);
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
+var claim6 = new Claim('Anthony Maki', 'Primary Care', 357);
 
-var totalPayedOut = 0;
+var claim7 = new Claim('James Bond', 'Emergency', 300000);
 
+var claim8 = new Claim('Jeff Goldblum', 'Optical', 775);
+
+var claim9 = new Claim('Namey McNameface', 'Specialist', 11235813);
+
+var claim10 = new Claim('Mr. X', 'Optical', 12345);
+
+var initialList = [claim1, claim2, claim3, claim4, claim5];
+
+initialList.push(claim6, claim7, claim8, claim9, claim10);
+
+//	console.log('claims array:', initialList);
+
+var totalPaidOut = 0;
+
+//	Processing claims on load
+$(document).ready(function(){
+
+	var $main = $("main");
+
+	for (var i = 0; i < initialList.length; i++) {
+		$($main).append('<p></p>');
+
+		var $p = $("main p:last-child");
+
+		var claimPayout = amountCovered(percentCovered(initialList[i].visitType), initialList[i].visitCost);
+
+		var claimPayoutLocalized = claimPayout.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+
+		var claimPatient = initialList[i].patientName;
+
+		totalPaidOut += claimPayout;
+
+		console.log('Paid out $' + claimPayout + ' for ' + claimPatient);
+
+		$($p).append('Paid out <span class="label label-danger">' + claimPayoutLocalized + '</span> for <strong>' + claimPatient + '</strong>');
+	}
+
+	var totalPaidOutLocalized = totalPaidOut.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+
+	console.log('Total claim payouts: $' + totalPaidOut);
+
+	$($main).append('<h3><span class="label label-info">Total claim payouts</span> ' + totalPaidOutLocalized + '</h3>');
+});
+
+//	FUNCTION DECLARATIONS
+//	Constructor for claims
 function Claim(name, type, cost){
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
 }
 
-//function to determine percent covered
+//	Function to determine percent covered
+function percentCovered(claimType){
+	var percentCovered = 0;
 
-//function to determine amount covered
+	switch(claimType){
+		case 'Emergency':
+			percentCovered = 100;
+			break;
+		case 'Optical':
+			percentCovered = 0;
+			break;
+		case 'Primary Care':
+			percentCovered = 50;
+			break;
+		case 'Specialist':
+			percentCovered = 10;
+			break;
+		default:
+			percentCovered = 0;
+			break;
+	}
+
+	return percentCovered;
+}
+
+//	Function to determine amount covered
+function amountCovered(percentCovered, claimAmount){
+	var amountCovered = parseInt((claimAmount * percentCovered / 100).toFixed());
+
+	return amountCovered;
+}
