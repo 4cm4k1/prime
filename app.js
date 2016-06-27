bbb815c9bf99039a17e3b03ea17b69ce1f0306d7
Anthony Maki <4cm4k1@gmail.com>
2016-06-27 16:41:08 -0500

    Add vendor sources, comment outline of planned HTML and JS code

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+//  car records array
+
+//  HARD MODE: random car array
+
 $(function(){
 
+  //  Event listener for form submission
+  $("main").on('click', '#submit', function(){
+    //  Form handler function called
+      //  Constructor function called
+      //  refreshDOM function called
+        //  color indicator update function called
+        //  HARD MODE: alphabet sort function called
+        //  PRO MODE: update price total function called
+  });
+
+  //  Event listener for record deletion
+  $("main").on('click', '#delete', function(){
+    //  Remove records function called
+      //  refreshDOM function called
+        //  color indicator update function called
+        //  HARD MODE: alphabet sort function called
+        //  PRO MODE:  update price total function called
+  });
+
 });
+
+//  Constructor function for car records
+
+//  Form handler function
+
+//  Function that clears the DOM and appends the contents of the records array
+function refreshDOM(){
+
+}
+
+//  Function that removes records from array (refreshDOM will reflect change)
+
+//  Function that updates the color indicator for review star rating
+
+//  HARD MODE: random car array picker function
+
+//  HARD MODE: alphabet sort function
