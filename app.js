a1b8f09046480c7f954f77f352bed4fcd40b87da
Anthony Maki <4cm4k1@gmail.com>
2016-06-30 02:24:50 -0500

    Clean up extraneous comments and logs, restructure code to (1) variable (2) function declarations (3) functions executed on load (4) event listeners, add verbose comments explaining variables, functions, etc

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 $(function() {
-
+    //  VARIABLE DECLARATIONS
+    //  Initial value for number of teams desired is set to zero
+    var numTeams = 0;
+    //  Initial empty array set, in which the generated team arrays will be pushed
+    var teams = [];
+    //  Array of students in the class to be randomly split into teams
     var studentArray = [
         "Richard",
         "Jon",
@@ -21,11 +26,13 @@ $(function() {
         "Katie",
         "Trent"
     ];
-
-    var numTeams = 0;
-    var teams = [];
+    //  Max number of teams that would be reasonable for total number of
+    //  students (because, for example, 20 students logically could only be
+    //  placed into 10 teams of 2)
     var maxNumber = parseInt(studentArray.length / 2);
 
+    //  FUNCTION DECLARATIONS
+    //  Takes a given array and reorders its contents in randomized order
     function randomizeArray(array) {
         var i, num, temp;
         for (i = array.length; i; i--) {
@@ -34,30 +41,30 @@ $(function() {
             array[i - 1] = array[num];
             array[num] = temp;
         }
+        console.log("Here's your initial array, but randomized:", array);
     }
 
+    //  Adds dynamic number of buttons to DOM based on how many teams that
+    //  would logically and reasonably be wanted (for example, for 19 people
+    //  it would be between 2 and 9 teams)
     function appendButtons() {
-        for (var i = 0; i < maxNumber; i++) {
+        for (var i = 1; i < maxNumber; i++) {
             $("#numberOfTeams").append("<button type='button' class='btn btn-lg btn-default' id='numTeamsChosen'>" + (i + 1) + "</button>");
         }
     }
 
+    //  Returns an array of arrays of randomized teams
     function makeTeams() {
-        // var divNum=parseInt(studentArray.length/numTeams);
-        // var remainder=studentArray.length % numTeams;
         var tempArray = [];
         tempArray = chunkify(studentArray, numTeams, true);
-        console.log(tempArray);
-        // for (i=0; i<studentArray.length; i+=divNum){
-        //   if(divNum > studentArray.length) {
-        //
-        //   }
-        //   tempArray.push(studentArray.slice(i, divNum));
-        //   divNum+=divNum+1;
-        // }
+        console.log("Here's your array of randomized teams:", tempArray);
         return tempArray;
     }
 
+    //  Takes in array (already randomized) that you want to chunkify, the
+    //  number of chunks that you want, and whether you want the chunks to be
+    //  similarly sized as a boolean value (false will likely give you a team
+    //  with only one person)
     function chunkify(arrayYouWantToChunkify, howManyChunksYouWant, doYouWantChunksOfSimilarSize) {
         if (howManyChunksYouWant < 2)
             return [arrayYouWantToChunkify];
@@ -85,9 +92,12 @@ $(function() {
             }
             out.push(arrayYouWantToChunkify.slice(size * howManyChunksYouWant));
         }
+        console.log("Here's your chunkified array of randomized teams:", out);
         return out;
     }
 
+    //  Clears div in DOM that lists teams, and reappends the teams according
+    //  to the current value of the teams array
     function appendTeams() {
         $("#teamList").empty();
         for (var i = 0; i < teams.length; i++) {
@@ -103,25 +113,24 @@ $(function() {
         }
     }
 
-    $("#randomize").on("click", function(event) {
-        event.preventDefault();
-        randomizeArray(studentArray);
-        teams = makeTeams();
-        console.log(teams);
-        appendTeams();
-    });
+    //  FUNCTIONS TO EXECUTE ON LOAD
+    appendButtons();
 
+    //  EVENT LISTENERS
+    //  Event listener for button click for number of teams desired
     $("#numberOfTeams").on("click", "#numTeamsChosen", function(event) {
         event.preventDefault();
         numTeams = $(this).text();
-        console.log(this);
+        console.log("You want", $(this).text(), "randomized teams");
     });
 
-    appendButtons();
-    // var divNum=studentArray.length/numTeams;
-    // for (i=0; i<studentArray.length; i+=divNum){
-    //   studentArray.slice(i, divNum);
-    //   divNum+=divNum;
-    // }
-
+    //  Event listener for button click for generating randomized teams
+    $("#randomize").on("click", function(event) {
+        //  TODO: Create logic checking if numTeams = 0, then alert user
+        //        that they need to first pick desired number of teams
+        event.preventDefault();
+        randomizeArray(studentArray);
+        teams = makeTeams();
+        appendTeams();
+    });
 });
