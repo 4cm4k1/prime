5aac942b154486305e56ef4c8b89ac4a9f315cb2
Anthony Maki <4cm4k1@gmail.com>
2016-07-05 05:04:18 -0500

    Complete basic mode, less optional tag input

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 $(function(){
-  console.log('I am working!');
+
+  //  Random GIF Button Event Listener
+  $("#randomGIF").on("click", getRandomGIF);
 });
+
+var giphyAPIkey = 'dc6zaTOxFJmzC';
+var randomGIFobj = {};
+var randomGIFheight = 0;
+var randomGIFwidth = 0;
+var randomGIFsrc = '';
+
+function getRandomGIF() {
+  randomGIFobj = $.get("http://api.giphy.com/v1/gifs/random?api_key=" + giphyAPIkey)
+  .done(function(){
+    console.log('This is what I got back:', randomGIFobj);
+  })
+  .always(function(){
+    randomGIFheight = randomGIFobj.responseJSON.data.image_height;
+    randomGIFwidth = randomGIFobj.responseJSON.data.image_width;
+    randomGIFsrc = randomGIFobj.responseJSON.data.image_url;
+    showRandomGIF();
+    console.log('end of always');
+  });
+}
+
+function showRandomGIF() {
+  var $gifDiv = $("#allTheGIFs");
+  $gifDiv.empty();
+  $gifDiv.append("<img/>");
+  $gifDiv.find("img").attr("src", randomGIFsrc);
+  $gifDiv.find("img").attr("height", randomGIFheight);
+  $gifDiv.find("img").attr("width", randomGIFwidth);
+  $gifDiv.find("img").attr("class", "img-responsive img-rounded");
+}
