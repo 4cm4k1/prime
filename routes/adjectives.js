e98a56fc8526d6aba93e9b7a8b840d1b58fb477c
Megan Martinson <hello@meg.codes>
2016-07-19 10:22:12 -0500

    fixed braces and semicolons

--- routes/adjectives.js
+++ routes/adjectives.js
@@ -6,27 +6,27 @@ var config = {
 }
 
 router.get('/', function(request, response) {
-    var client = new pg.Client(config);
-    client.connect(function(err) {
-        if (err) {
-            console.log("there was a problem", error);
-        }
-        client.query("SELECT * FROM adjectives", function(err, result) {
-            if (err) {
-                console.log("there was another problem", error);
-            } else {
-                console.log("Go team!");
-                console.log(result.rows);
-                response.send(result.rows);
-              }
-            client.end(function(err) {
+            var client = new pg.Client(config);
+            client.connect(function(err) {
                 if (err) {
-                    console.log("There was a disconnection problem!")
+                    console.log("there was a problem", error);
                 }
-            })
-        });
+                client.query("SELECT * FROM adjectives", function(err, result) {
+                    if (err) {
+                        console.log("there was another problem", error);
+                    } else {
+                        console.log("Go team!");
+                        console.log(result.rows);
+                        response.send(result.rows);
+                    }
+                    client.end(function(err) {
+                        if (err) {
+                            console.log("There was a disconnection problem!")
+                        }
+                    });
+                });
+            });
 });
 
 
-
-module.exports = router;
+            module.exports = router;
