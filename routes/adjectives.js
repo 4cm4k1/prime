61f206beb86d3124f32268218ea5746a83c30c41
Megan Martinson <hello@meg.codes>
2016-07-19 10:08:43 -0500

    routers done

---
+++ routes/adjectives.js
@@ -0,0 +1,36 @@
+var router = require('express').Router();
+var pg = require('pg');
+var config = {
+    database: "twittergen",
+    port: 5432
+}
+
+router.get('/', function(request, response) {
+    var client = new pg.Client(config);
+    client.connect(function(err) {
+        if (err) {
+            console.log("there was a problem", error);
+        }
+        client.query("SELECT * FROM adjectives", function(err, result) {
+            if (err) {
+                console.log("there was another problem", error);
+            } else {
+                console.log("Go team!");
+                console.log(result.rows);
+                response.send(result.rows);
+              }
+            client.end(function(err) {
+                if (err) {
+                    console.log("There was a disconnection problem!")
+                }
+            })
+        });
+
+    });
+    // response.send =
+        // client.
+});
+
+
+
+module.exports = router;
