74c06b2dcdece168facac8544cd9ade5f5003d08
forente <zerofox16@gmail.com>
2016-07-20 14:53:59 -0500

    people in table 2

--- database.sql
+++ database.sql
@@ -31,3 +31,26 @@ VALUES ('Ostrich');
 
 INSERT INTO patronuses (patronus_name)
 VALUES ('Emu');
+
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Henry','Haggar',1);
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Taylor','Kabe',2);
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Rupert','Blu',3);
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Lala','Lark', 4);
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Mikki','Ray', 5);
+
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Nikki','Button',6);
+
+INSERT INTO people (first_name, last_name, patronus_id)
+VALUES ('Leblane','Jones',2);
