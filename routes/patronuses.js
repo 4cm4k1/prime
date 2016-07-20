9b97c2bb0f51d54387c5e55ac5b652fd4608d027
Anthony Maki <4cm4k1@gmail.com>
2016-07-20 14:23:18 -0500

    Add post methods for each route

--- routes/patronuses.js
+++ routes/patronuses.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var pg = require('pg');
 var config = {
-    database: "patronusRegistry",
+    database: 'patronusRegistry',
     port: 5432
 };
 
@@ -11,17 +11,42 @@ router.get('/', function(request, response) {
 
     client.connect(function(err) {
         if (err) {
-            console.log("ERROR: You need to think of a time you were truly happy...", err);
+            console.log('ERROR: You need to think of a time you were truly happy...', err);
         }
-        client.query("SELECT * FROM patronuses", function(err, result) {
+        client.query('SELECT * FROM patronuses', function(err, result) {
             if (err) {
-                console.log("ERROR: Really, try to think happy...", err);
+                console.log('ERROR: Really, try to think happy...', err);
             } else {
                 response.send(result.rows);
             };
             client.end(function(err) {
                 if (err) {
-                    console.log("ERROR: Your patronus incantation was interrupted...", err);
+                    console.log('ERROR: Your patronus incantation was interrupted...', err);
+                }
+            })
+        });
+
+    });
+});
+
+router.post('/', function(request, response) {
+    var client = new pg.Client(config);
+
+    var patronusName = req.body.patronusName; //  adjust for incoming object
+
+    client.connect(function(err) {
+        if (err) {
+            console.log('ERROR: You need to think of a time you were truly happy...', err);
+        }
+        client.query('INSERT INTO patronuses (patronus_name) VALUES ($1)', [patronusName], function(err, result) {
+            if (err) {
+                console.log('ERROR: Really, try to think happy...', err);
+            } else {
+                response.send(result.rows);
+            };
+            client.end(function(err) {
+                if (err) {
+                    console.log('ERROR: Your patronus incantation was interrupted...', err);
                 }
             })
         });
--- routes/people.js
+++ routes/people.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var pg = require('pg');
 var config = {
-    database: "patronusRegistry",
+    database: 'patronusRegistry',
     port: 5432
 };
 
@@ -11,17 +11,46 @@ router.get('/', function(request, response) {
 
     client.connect(function(err) {
         if (err) {
-            console.log("ERROR: You need to think of a time you were truly happy...", err);
+            console.log('ERROR: You need to think of a time you were truly happy...', err);
         }
-        client.query("SELECT * FROM people", function(err, result) {
+        client.query('SELECT * FROM people', function(err, result) {
             if (err) {
-                console.log("ERROR: Really, try to think happy...", err);
+                console.log('ERROR: Really, try to think happy...', err);
             } else {
                 response.send(result.rows);
             };
             client.end(function(err) {
                 if (err) {
-                    console.log("ERROR: Your patronus incantation was interrupted...", err);
+                    console.log('ERROR: Your patronus incantation was interrupted...', err);
+                }
+            })
+        });
+
+    });
+});
+
+router.post('/', function(request, response) {
+    var client = new pg.Client(config);
+
+    var person = {
+      firstName: req.body.person.firstName,
+      lastName: req.body.person.lastName,
+      patronusID: req.body.person.patronusID
+    }; //  adjust for incoming object
+
+    client.connect(function(err) {
+        if (err) {
+            console.log('ERROR: You need to think of a time you were truly happy...', err);
+        }
+        client.query('INSERT INTO people (first_name, last_name, patronus_id) VALUES ($1, $2, $3)', [person.firstName, person.lastName, person.patronusID], function(err, result) {
+            if (err) {
+                console.log('ERROR: Really, try to think happy...', err);
+            } else {
+                response.send(result.rows);
+            };
+            client.end(function(err) {
+                if (err) {
+                    console.log('ERROR: Your patronus incantation was interrupted...', err);
                 }
             })
         });
