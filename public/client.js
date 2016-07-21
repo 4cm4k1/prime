e4e397761479bc9a74b8348043dfe6d38a88d1bc
Anthony Maki <4cm4k1@gmail.com>
2016-07-21 04:34:23 -0500

    Finish challenge

--- public/client.js
+++ public/client.js
@@ -1,33 +1,65 @@
-angular.module('angularOlympics', []);
+angular.module('angularOlympics', ['ngRoute']);
 
-angular.module('angularOlympics').controller('MainController', ['$scope', '$http', function($scope, $http) {
-    //  array of strings to use for routes
-    $scope.sports = ['archery', 'canoe-kayak', 'modern-pentathlon', 'soccer', 'taekwondo'];
+angular.module('angularOlympics').config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/archery', {
+            templateUrl: 'views/archery.html',
+            controller: 'ArcheryController'
+        })
+        .when('/canoe-kayak', {
+            templateUrl: 'views/canoe-kayak.html',
+            controller: 'CanoeKayakController'
+        })
+        .when('/modern-pentathlon', {
+            templateUrl: 'views/modern-pentathlon.html',
+            controller: 'ModernPentathlonController'
+        })
+        .when('/soccer', {
+            templateUrl: 'views/soccer.html',
+            controller: 'SoccerController'
+        })
+        .when('/taekwondo', {
+            templateUrl: 'views/taekwondo.html',
+            controller: 'TaekwondoController'
+        })
+    $locationProvider.html5Mode(true);
+});
 
-    //  ng-click function
-    $scope.generateAthletes = function() {
-        makeGetRequests($scope.sports);
-    };
+angular.module('angularOlympics').controller('ArcheryController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/olympics/archery').then(function handleSuccess(response) {
+        $scope.athlete = response.data;
+    }, function handleFailure(response) {
+        console.log('Error:', response);
+    });
+}]);
 
-    //  this function executes the $http requests and stores the responses
-    //  to $scope.athleteList
-    function makeGetRequests(sportsArray) {
-        var athleteList = [];
-        angular.forEach(sportsArray, function(item) {
-            var request = $http({
-                method: 'GET',
-                url: '/olympics/' + item
-            }).then(handleSuccess, handleFailure)
+angular.module('angularOlympics').controller('CanoeKayakController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/olympics/canoe-kayak').then(function handleSuccess(response) {
+        $scope.athlete = response.data;
+    }, function handleFailure(response) {
+        console.log('Error:', response);
+    });
+}]);
 
-            function handleSuccess(response) {
-                athleteList.push(response.data);
-                console.log('Success:', response.data);
-            }
+angular.module('angularOlympics').controller('ModernPentathlonController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/olympics/modern-pentathlon').then(function handleSuccess(response) {
+        $scope.athlete = response.data;
+    }, function handleFailure(response) {
+        console.log('Error:', response);
+    });
+}]);
 
-            function handleFailure(response) {
-                console.log('Error:', response.data);
-            }
-        });
-        $scope.athleteList = athleteList;
-    }
+angular.module('angularOlympics').controller('SoccerController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/olympics/soccer').then(function handleSuccess(response) {
+        $scope.athlete = response.data;
+    }, function handleFailure(response) {
+        console.log('Error:', response);
+    });
+}]);
+angular.module('angularOlympics').controller('TaekwondoController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/olympics/taekwondo').then(function handleSuccess(response) {
+        $scope.athlete = response.data;
+    }, function handleFailure(response) {
+        console.log('Error:', response);
+    });
 }]);
