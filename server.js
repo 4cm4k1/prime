ae26b538f3965f828491ca4ac51586d6bb39c19a
Anthony Maki <4cm4k1@gmail.com>
2016-07-22 13:55:45 -0500

    Finish server.js and add assignments and index routes

---
+++ server.js
@@ -0,0 +1,30 @@
+//  requires
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//  routes
+var index = require('./routes/index');
+var assignments = require('./routes/assignments');
+
+//  bodyParser config
+app.use(bodyParser.json());
+
+//  routing
+app.use(express.static('public'));
+app.use('/', index);
+app.use('/assignments', assignments);
+
+//  database
+var db = mongoose.connect('mongodb://localhost/something').connection;
+
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+//  server on/port listening
+var server = app.listen(process.env.PORT || 3000, function(){
+  var port = server.address().port;
+  console.log('Listening on port', port);
+});
